22ed8b1fc71335408353432a41d7875c2342bdc4
wellington <wellingtonslpbi@hotmail.com>
2020-11-29 15:50:24 -0300

    adding apollo client into project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { ApolloProvider } from '@apollo/client';
+
 import Routes from './routes';
+import client from './services/api';
 
 const App: React.FC = () => (
-  <NavigationContainer>
-    <Routes />
-  </NavigationContainer>
+  <ApolloProvider client={client}>
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
+  </ApolloProvider>
 );
 
 export default App;
