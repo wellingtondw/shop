f47ab4987bc1e5bda5e5d876bd0f342615e07ca5
wellington <wellingtonslpbi@hotmail.com>
2020-11-29 13:01:17 -0300

    adding eslint to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React from 'react';
+import { Text } from 'react-native';
 
-const App = () => {
-  return (
-    <Text>Hello world</Text>
-  );
-};
+const App = () => (
+  <Text>Hello world</Text>
+);
 
 export default App;
