0d5cc75497f27ba7aabb36964a23f9a2fe84c0b4
wellington <wellingtonslpbi@hotmail.com>
2020-11-29 15:23:48 -0300

    adding navigation into project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Routes from './routes';
 
-const App = () => <Text>Hello world</Text>;
+const App: React.FC = () => (
+  <NavigationContainer>
+    <Routes />
+  </NavigationContainer>
+);
 
 export default App;
