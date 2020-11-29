0b91413f6ff570c12f4e9f078ce2c762368c6b7b
wellington <wellingtonslpbi@hotmail.com>
2020-11-29 17:06:01 -0300

    adding styled-components into project

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Container, Title } from './styles';
 
-const Home: React.FC = () => <Text>Home</Text>;
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <Title>Home</Title>
+    </Container>
+  );
+};
 
 export default Home;
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background: #ccc;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  color: #000;
+`;
