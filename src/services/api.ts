1d3f69e14e1a171e5db83f1e8d681aa8f8b8b3ff
wellington <wellingtonslpbi@hotmail.com>
2020-11-30 19:43:03 -0300

    adjust api endpoint to android emulator

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  uri: 'http://10.0.2.2:3000/graphql',
   cache: new InMemoryCache(),
 });
 
