b984fd0c1e2b3abd633792a24e7fc1c814272cec
wellington <wellingtonslpbi@hotmail.com>
2020-11-30 22:24:13 -0300

    create theme file

---
+++ src/styles/theme.ts
@@ -0,0 +1,33 @@
+export default {
+  border: {
+    radius: '2px',
+  },
+  font: {
+    normal: 'Rubik-Regular',
+    medium: 'Rubik-Medium',
+    bold: 'Rubik-Bold',
+    sizes: {
+      normal: '15px',
+      medium: '20px',
+    },
+    lineHeights: {
+      normal: '20px',
+      medium: '24px',
+    },
+  },
+  colors: {
+    primary: '#333333',
+    secondary: '#637381',
+    green: '#1C9956',
+    red: '#BF1D08',
+  },
+  spacings: {
+    xxsmall: '2px',
+    xsmall: '4px',
+    small: '8px',
+    medium: '16px',
+    large: '24px',
+    xlarge: '32px',
+    xxlarge: '40px',
+  },
+};
