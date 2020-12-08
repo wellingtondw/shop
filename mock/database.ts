01c2e40172036ccdce2495bf890ae4498dd10962
wellington <wellingtonslpbi@hotmail.com>
2020-12-07 21:37:41 -0300

    change script to run api server and create mock/database file

---
+++ mock/database.ts
@@ -0,0 +1,571 @@
+module.exports = {
+  skus: [
+    {
+      id: 1,
+      name: "Arquitetura de Computadores",
+      ean: "9788521616603",
+      category: "Livros",
+      imageUrl:
+        "https://images-shoptime.b2w.io/produtos/01/00/sku/7134/2/7134233P.jpg",
+      description:
+        "A melhor forma de otimizar o aproveitamento da capacidade de um computador é compreender os princípios básicos do seu funcionamento",
+      package: {
+        height: 3,
+        width: 20,
+        depth: 28,
+        weight: 1307,
+      },
+      salePrice: 12990,
+      promotionalPrice: 8990,
+      quantity: 3,
+    },
+    {
+      id: 2,
+      name: "Teclado Xz-530/Waterproof Com Fio USB - Teclado Padrão",
+      ean: "6955727788134",
+      category: "Acessórios de informática",
+      imageUrl:
+        "https://images-submarino.b2w.io/produtos/01/00/sku/41321/1/41321177P.jpg",
+      description:
+        "USB 2.0 Resolução de 1.000 dpi Waterproof Suporte de inclinação Nove Orifícios de proteção",
+      package: {
+        height: 1,
+        width: 12,
+        depth: 112,
+        weight: 1000,
+      },
+      salePrice: 11990,
+      promotionalPrice: 13990,
+      quantity: 1,
+    },
+    {
+      id: 3,
+      name: "Jogo Game Of Life Meu Malvado Favorito - Hasbro",
+      ean: "5010994844899",
+      category: "Brinquedos",
+      imageUrl:
+        "https://images-shoptime.b2w.io/produtos/01/00/oferta/45611/8/45611896P1.jpg",
+      description:
+        "Entre nessa missão com os Minions! No jogo Game of Life Meu Malvado Favorito seja o primeiro a pegar 5 cachos de banana para vencer!",
+      package: {
+        height: 100,
+        width: 100,
+        depth: 100,
+        weight: 1000,
+      },
+      salePrice: 21490,
+      promotionalPrice: 21490,
+      quantity: 10,
+    },
+    {
+      id: 4,
+      name: "Pista e Veículos - Metal Machines - Construction - Candide",
+      ean: "7897500587031",
+      category: "Brinquedos",
+      imageUrl:
+        "https://images-submarino.b2w.io/produtos/01/00/images/29954/0/29954072P1.jpg",
+      description:
+        "Os meninos vão passar horas se divertindo com essa irada Pista e Veículos - Metal Machines - Construction da Candide. A Pista e Veículos - Metal Machines - Construction da Candide",
+      package: {
+        height: 200,
+        width: 1100,
+        depth: 1600,
+        weight: 3000,
+      },
+      salePrice: 25490,
+      promotionalPrice: 25490,
+      quantity: 8,
+    },
+    {
+      id: 5,
+      name: "Panela de Arroz 10 xícaras ML-2900 NKS Milano 220V",
+      ean: "7896843933956",
+      category: "Eletroportáteis",
+      imageUrl:
+        "https://images-submarino.b2w.io/produtos/01/00/oferta/41283/4/41283451P1.jpg",
+      description:
+        "A panela de arroz ML-2900 NKS Milano possui capacidade de 1,8 litros e 10 xícaras e é ideal para quem gosta de praticidade! Prepara um delicioso arroz de maneira saudável e rápida.",
+      package: {
+        height: 100,
+        width: 100,
+        depth: 100,
+        weight: 1000,
+      },
+      salePrice: 12900,
+      promotionalPrice: 15900,
+      quantity: 2,
+    },
+    {
+      id: 6,
+      name: "Ducha Lorenzetti Relax 3t 220v 5500w Branca",
+      ean: "7896451826763",
+      category: "Banheiro",
+      imageUrl:
+        "https://images-submarino.b2w.io/produtos/01/00/image/79029/6/79029662P1.jpg",
+      description:
+        "Ducha Lorenzetti Relax 3T 220v 5500w Branca. Código Do Produto: BT224448RF. Fabricante Lorenzetti. O design marcante da ducha Relax dá um banho de estilo no seu banho",
+      package: {
+        height: 18,
+        width: 26,
+        depth: 28,
+        weight: 3000,
+      },
+      salePrice: 8400,
+      promotionalPrice: 8400,
+      quantity: 15,
+    },
+    {
+      id: 7,
+      name: "Telefone Intelbras Sem Fio Ts40 Sem Identificador De Chamadas",
+      ean: "7896637625661",
+      category: "Telefonia Fixa",
+      imageUrl: null,
+      description:
+        "<p>Características Técnicas:<br> <br> Tecnologia Dect: maior alcance e livre de interferência<br> Design premiado<br> 2 teclas de discagem direta",
+      package: {
+        height: 9,
+        width: 18,
+        depth: 13,
+        weight: 3000,
+      },
+      salePrice: 13250,
+      promotionalPrice: 13250,
+      quantity: 1,
+    },
+    {
+      id: 8,
+      name: "Livro - 12 Years A Slave",
+      ean: "9781910173527",
+      category: "Livros",
+      imageUrl:
+        "https://images-shoptime.b2w.io/produtos/01/00/sku/128342/4/128342405G1.jpg",
+      description:
+        "Doze Anos de Escravidão é a extraordinária história real de Solomon Northup, um homem negro livre que morava no estado de New York em 1841 quando foi sequestrado e vendido como escravo",
+      package: {
+        height: 21,
+        width: 14,
+        depth: 20,
+        weight: 136,
+      },
+      salePrice: 2990,
+      promotionalPrice: 2390,
+      quantity: 20,
+    },
+    {
+      id: 9,
+      name: "Óculos Shimano AeroLite Ce-ARLT1-Mr Preto Fosco 2 Lentes",
+      ean: "4524667692775",
+      category: "Esporte",
+      imageUrl:
+        "https://images-submarino.b2w.io/produtos/01/00/sku/34869/5/34869530P1.jpg",
+      description:
+        "O Óculos Shimano AeroLite é a versão para desempenho de nível profissional, o seu desing sem montagem desenvolvido em colaboração com a equipe LottoNL-Jumbo.",
+      package: {
+        height: 100,
+        width: 100,
+        depth: 100,
+        weight: 1000,
+      },
+      salePrice: 41900,
+      promotionalPrice: 41900,
+      quantity: 0,
+    },
+    {
+      id: 10,
+      name: "Terra Cortril Spray 125ml",
+      ean: "7898049717705",
+      category: "Pet shop",
+      imageUrl:
+        "https://images-submarino.b2w.io/produtos/01/00/oferta/46993/8/46993809P1.jpg",
+      description:
+        "<br> <p>Terra-Cortril - Antibiótico anti-inflamatório indicado ao tratamento de querato conjuntivite infecciosa (pink eye) de bovinos e ovinos; afecções oculares e cutâneas, eczemas, queimaduras...",
+      package: {
+        height: 100,
+        width: 100,
+        depth: 100,
+        weight: 1000,
+      },
+      salePrice: 350,
+      promotionalPrice: 3500,
+      quantity: 3,
+    },
+    {
+      id: 11,
+      name: "Controle Ps3 Wireless Inova",
+      ean: "9788577226511",
+      category: "Consoles e Games",
+      imageUrl:
+        "https://images-submarino.b2w.io/produtos/01/00/oferta/50083/2/50083208G1.jpg",
+      description:
+        "Controle PS 3 Wireless&lt;br /&gt;&lt;br /&gt;&lt;br /&gt;&lt;br /&gt; Tenha mais precisão e mobilidade durante suas jogadas com o controle DualShock para Playstation 3.",
+      package: {
+        height: 9,
+        width: 15,
+        depth: 3,
+        weight: 300,
+      },
+      salePrice: 4900,
+      promotionalPrice: 4900,
+      quantity: 1,
+    },
+    {
+      id: 12,
+      name: "Massa Areia Para Modelar Sunny Marrom Natural - 1804",
+      ean: "7899573618049",
+      category: "Brinquedos",
+      imageUrl:
+        "https://images-shoptime.b2w.io/produtos/01/00/img1/33130/3/33130334G1.jpg",
+      description:
+        "Massa Areia Para Modelar Sunny Marrom Natural - 1804&lt;br&gt;Itens Inclusos: 1 Pacote de Massa Areia.&lt;br&gt;Recomendação: Recomendado para crianças maiores de 3 anos de idade.",
+      package: {
+        height: 1,
+        width: 12,
+        depth: 112,
+        weight: 1000,
+      },
+      salePrice: 13990,
+      promotionalPrice: 11990,
+      quantity: 1,
+    },
+    {
+      id: 13,
+      name:
+        "Figura de Ação 19 Cm - Rescue Heroes - Billy Blazers - Bombeiro - Mattel",
+      ean: "5010994844899",
+      category: "Brinquedos",
+      imageUrl: null,
+      description:
+        "A diversão esta mais que garantida com a Figura de Ação 19 Cm - Rescue Heroes - Billy Blazers - Bombeiro - Mattel. Billy Blazers é um bombeiro especialista e o líder da equipe Rescue Heroes. ",
+      package: {
+        height: 23,
+        width: 10,
+        depth: 23,
+        weight: 1500,
+      },
+      salePrice: 11999,
+      promotionalPrice: 11999,
+      quantity: 12,
+    },
+    {
+      id: 14,
+      name: "Suporte Fixo Para Tv E Monitores",
+      ean: "7898959754012",
+      category: "Eletrônicos",
+      imageUrl:
+        "https://images-submarino.b2w.io/produtos/01/00/img/44216/0/44216033G1.jpg",
+      description:
+        "Suporte de TV e Monitores LED / LCD&lt;/p&gt;&lt;p&gt;Universal e Fixo&lt;br /&gt;Suporta TVs de 10 à 71 Polegadas&lt;br /&gt;Suporte para Monitor de Computador&lt;br /&gt;Compatível com todas as TVs e Monitores",
+      package: {
+        height: 16,
+        width: 4,
+        depth: 8,
+        weight: 3000,
+      },
+      salePrice: 5000,
+      promotionalPrice: 5000,
+      quantity: 1,
+    },
+    {
+      id: 15,
+      name: "As Grandes Batalhas da II Guerra Mundial",
+      ean: "9788538902447",
+      category: "Livros",
+      imageUrl:
+        "https://images-submarino.b2w.io/produtos/01/00/oferta/31185/4/31185485G1.jpg",
+      description:
+        "O texto descreve a progressão da grande guerra, que começou como um conflito limitado a uma pequena parte da Europa e se expandiu, envolvendo países de todos os continentes",
+      package: {
+        height: 2,
+        width: 20,
+        depth: 26,
+        weight: 100,
+      },
+      salePrice: 3990,
+      promotionalPrice: 3990,
+      quantity: 2,
+    },
+    {
+      id: 16,
+      name:
+        "SSD Kingston A400 240GB - 500mb/s para Leitura e 350mb/s para Gravação",
+      ean: "0740617261219",
+      category: "PC Gamer",
+      imageUrl:
+        "https://images-americanas.b2w.io/produtos/01/00/sku/133764/4/133764426G1.jpg",
+      description:
+        "Armazene fotos, videos, musicas e documentos, com muito mais segurança e sem comprometer a memória do seu computador com o &lt;b&gt;SSD Kingston A400 240GB",
+      package: {
+        height: 2,
+        width: 20,
+        depth: 26,
+        weight: 100,
+      },
+      salePrice: 3990,
+      promotionalPrice: 3990,
+      quantity: 2,
+    },
+    {
+      id: 17,
+      name: "Pen Drive Usb 16gb Otg 3em1 C/cartao Microsd Unidade U-tech",
+      ean: "7898597390658",
+      category: "Eletrônicos",
+      imageUrl:
+        "https://images-shoptime.b2w.io/produtos/01/00/oferta/56869/1/56869194G1.jpg",
+      description:
+        "Pen Drive + Cartão de Memória Micro-Sd Otg U-Tech 16GB PretoTransfira arquivos facilmente de seu smartphone ou tablet diretamente para um computador. Sistema Operacional Compatível: Linux, Macintosh, Microsoft Windows",
+      package: {
+        height: 1,
+        width: 10,
+        depth: 12,
+        weight: 300,
+      },
+      salePrice: 4500,
+      promotionalPrice: 4500,
+      quantity: 1,
+    },
+    {
+      id: 18,
+      name: "Livro - Sol da meia-noite: (Midnight Sun) - Série Crepúsculo",
+      ean: "9786555600292",
+      category: "Livros",
+      imageUrl:
+        "https://images-soubarato.b2w.io/produtos/imagens/1708602357/1708602365G1.jpg",
+      description:
+        "Aguardado há mais de uma década, &lt;i&gt;Sol da meia-noite&lt;/i&gt;, novo livro do universo de &lt;i&gt;Crepúsculo&lt;/i&gt;, chega ao Brasil em lançamento mundial no dia 4 de agosto",
+      package: {
+        height: 23,
+        width: 16,
+        depth: 4,
+        weight: 1030,
+      },
+      salePrice: 5490,
+      promotionalPrice: 5490,
+      quantity: 2,
+    },
+    {
+      id: 19,
+      name: "Crec Crec Vamos Lanchar Big Star",
+      ean: "7896575102439",
+      category: "Brinquedos",
+      imageUrl: null,
+      description:
+        "Crec Crec Vamos Lanchar - Big Star   Indicado para crianças acima de 3 anos.Tem um momento na brincadeira que sempre dá aquela fominha, e nessa hora a gente preparou um brinquedo especial: Vamos Lanchar Crec-Crec!",
+      package: {
+        height: 27,
+        width: 6,
+        depth: 35,
+        weight: 3000,
+      },
+      salePrice: 4900,
+      promotionalPrice: 4900,
+      quantity: 1,
+    },
+    {
+      id: 20,
+      name: "Cartão Micro Sd 16gb Classe 10",
+      ean: "0619659161606",
+      category: "Cartões de memória",
+      imageUrl:
+        "https://images-submarino.b2w.io/produtos/01/00/oferta/34799/4/34799450G1.jpg",
+      description:
+        "Itens Inclusos:&lt;br&gt;-1 Cartão Micro Sd 16gb Sandisk Classe10 Ultra Speed 80mbs&lt;br&gt;&lt;br&gt;Capacidade: 16 Gb&lt;br&gt;Velocidade de Leitura: até 80MB/S",
+      package: {
+        height: 3,
+        width: 16,
+        depth: 11,
+        weight: 3,
+      },
+      salePrice: 7500,
+      promotionalPrice: 7500,
+      quantity: 1,
+    },
+    {
+      id: 21,
+      name: "Thomas e seus Amigos Track Master Pista Fuga do Dragao Fxx66",
+      ean: "0887961702972",
+      category: "Brinquedos",
+      imageUrl:
+        "https://images-submarino.b2w.io/produtos/01/00/img/593014/4/593014474G1.jpg",
+      description:
+        "A diversão esta garantida com a Pista Thomas e Seus Amigos Salto do Dragão Fisher-Price. Os trens motorizados TrackMaster e os conjuntos de trens permitem que as crianças experimentem um mundo de brincadeiras com momentos inspirados em Thomas e Friends.",
+      package: {
+        height: 33,
+        width: 16,
+        depth: 41,
+        weight: 3000,
+      },
+      salePrice: 16999,
+      promotionalPrice: 16999,
+      quantity: 12,
+    },
+    {
+      id: 22,
+      name: "Pendrive Multilaser Twist Preto 32Gb - PD589",
+      ean: "6955727788134",
+      category: "Acessórios de informática",
+      imageUrl:
+        "https://images-submarino.b2w.io/produtos/01/00/img/1445118/7/1445118706G1.jpg",
+      description:
+        "Características&lt;br /&gt; &lt;br /&gt;. Capacidade: 32 GB&lt;br /&gt; &lt;br /&gt;. Compatível: Mac OS 9.0 e &lt;br /&gt;superiores/ Windows 98 e superiores",
+      package: {
+        height: 1,
+        width: 8,
+        depth: 12,
+        weight: 20,
+      },
+      salePrice: 6000,
+      promotionalPrice: 6000,
+      quantity: 1,
+    },
+    {
+      id: 23,
+      name:
+        "Hot Wheels Mini Circuito De Corrida Mario Kart Original Mattel Ghk15",
+      ean: "0887961795042",
+      category: "Brinquedos",
+      imageUrl:
+        "https://images-submarino.b2w.io/produtos/01/00/img/1448085/3/1448085364G1.jpg",
+      description:
+        "Com a Pista de Percurso e Veículo Hot Wheels Mario Kart Circuito Lite Mattel a diversão é garantida. Com a Pista de Percurso e Veículo Hot Wheels Mario Kart Circuito Lite da Mattel, a imaginação corre em alta velocidade",
+      package: {
+        height: 38,
+        width: 56,
+        depth: 15,
+        weight: 3000,
+      },
+      salePrice: 41999,
+      promotionalPrice: 41999,
+      quantity: 12,
+    },
+    {
+      id: 24,
+      name: "Brinquedo Meu Chazinho 29 pçs - 3645",
+      ean: "7896647052600",
+      category: "Brinquedos",
+      imageUrl:
+        "https://images-submarino.b2w.io/produtos/01/00/img/1517761/4/1517761494G1.jpg",
+      description: "Meu Chazinho",
+      package: {
+        height: 39,
+        width: 30,
+        depth: 7,
+        weight: 3000,
+      },
+      salePrice: 3900,
+      promotionalPrice: 3900,
+      quantity: 1,
+    },
+    {
+      id: 25,
+      name: "Boneca Adora Doll Cats Meow - Bebe Reborn - 2020924",
+      ean: "0010475209242",
+      category: "Brinquedos",
+      imageUrl:
+        "https://images-shoptime.b2w.io/produtos/01/00/oferta/32286/0/32286033G1.jpg",
+      description: "Boneca Adora Doll Cats Meow - Bebe Reborn - 2020924",
+      package: {
+        height: 46,
+        width: 20,
+        depth: 13,
+        weight: 1000,
+      },
+      salePrice: 99999,
+      promotionalPrice: 99999,
+      quantity: 12,
+    },
+    {
+      id: 26,
+      name: "Boneca Baby Alive Bebê Passos e Sorrisos Loira Hasbro",
+      ean: "0630509827190",
+      category: "Brinquedos",
+      imageUrl: null,
+      description:
+        "A Boneca Baby Alive Passos e Sorrisos tem sapatos que se iluminam a cada passo!",
+      package: {
+        height: 40,
+        width: 15,
+        depth: 33,
+        weight: 3000,
+      },
+      salePrice: 21999,
+      promotionalPrice: 21999,
+      quantity: 12,
+    },
+    {
+      id: 27,
+      name: "Minhas Panelinhas Big Star",
+      ean: "7896575104419",
+      category: "Brinquedos",
+      imageUrl:
+        "https://images-submarino.b2w.io/produtos/01/00/oferta/10696/2/10696219G1.jpg",
+      description:
+        "Brincar de casinha vai ser ainda mais divertido com as Minhas Panelinhas. Chame as amiguinhas e comece a brincadeira.Composição: Plástico",
+      package: {
+        height: 39,
+        width: 12,
+        depth: 35,
+        weight: 39,
+      },
+      salePrice: 5900,
+      promotionalPrice: 5900,
+      quantity: 1,
+    },
+    {
+      id: 28,
+      name: "Game - Castlevania: Lords of Shadow - Collection - PS3",
+      ean: "0083717202868",
+      category: "Games",
+      imageUrl:
+        "https://images-submarino.b2w.io/produtos/01/00/sku/117272/7/117272780G1.jpg",
+      description:
+        "Edição de colecionador que inclui os títulos Castlevania Lord of Shadow, com DLCs x91Reveriex92 e x91Resurrectionx92, o 3DS Mirro of Fate e uma demo para o futuro lançamento Lords of Shadow 2",
+      package: {
+        height: 1,
+        width: 13,
+        depth: 13,
+        weight: 8,
+      },
+      salePrice: 7500,
+      promotionalPrice: 7500,
+      quantity: 1,
+    },
+    {
+      id: 29,
+      name: "Boneca Baby Alive Misturinha Sabor Tropical Loira Hasbro",
+      ean: "0083717202868",
+      category: "Brinquedos",
+      imageUrl:
+        "https://images-submarino.b2w.io/produtos/01/00/img/1872877/8/1872877803G1.jpg",
+      description:
+        "É muito divertido fazer papinhas para a bebê Misturinha com o liquidificador que funciona de verdade!",
+      package: {
+        height: 40,
+        width: 15,
+        depth: 30,
+        weight: 1400,
+      },
+      salePrice: 31999,
+      promotionalPrice: 31999,
+      quantity: 12,
+    },
+    {
+      id: 30,
+      name: "Game Mass Effect: Andromeda - PS4",
+      ean: "7892110208901",
+      category: "Games",
+      imageUrl:
+        "https://images-submarino.b2w.io/produtos/01/00/sku/131781/7/131781716G1.jpg",
+      description:
+        "Mass Effect: Andromeda - leva os jogadores para a galáxia de Andrômeda, muito além da Via Láctea. Lá, os jogadores vão liderar nossa luta por um novo lar em um território hostil assumindo a função de Explorador, líder de batedores com treino militar.",
+      package: {
+        height: 2,
+        width: 17,
+        depth: 13,
+        weight: 8,
+      },
+      salePrice: 7500,
+      promotionalPrice: 7500,
+      quantity: 1,
+    },
+  ],
+};
